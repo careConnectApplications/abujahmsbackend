32ea6994180e7415ca76422e32a7128a42f07435
Malachi Chidiebere Egbugha <malachi.egbugha@ekedp.com>
2024-12-13 12:53:44 +0100

    changeuserstatus

--- src/controllers/usermanagment/usermanagement.ts
+++ src/controllers/usermanagment/usermanagement.ts
@@ -1,7 +1,7 @@
 import { AnyExpression } from "mongoose";
 import configuration from "../../config";
-import  {readall,updateuser}  from "../../dao/users";
-import {encrypt} from "../../utils/otherservices";
+import  {readall,updateuser,readone}  from "../../dao/users";
+
 //get all users
 export async function getallusers(req:Request, res:AnyExpression){
     try{
@@ -19,6 +19,25 @@ export async function getallusers(req:Request, res:AnyExpression){
 
 }
 //deactivate a user
+export async function updatestatus(req:any, res:any){
+    const {id} = req.params;
+    try{
+        const response = await readone({_id:id});
+       const status= response?.status == configuration.userstatus[0]? configuration.userstatus[1]: configuration.userstatus[0];
+        const queryresult:any =await updateuser(id,{status});
+        res.status(200).json({
+            queryresult,
+            status:true
+          }); 
+
+    }
+    catch(e:any){
+        console.log(e);
+      res.status(403).json({status: false, msg:e.message});
+
+    }
+
+}
 
 //update a user
   export async function updateusers(req:any, res:any){
--- src/routes/usermanagement.ts
+++ src/routes/usermanagement.ts
@@ -1,11 +1,12 @@
 import express from 'express';
-import {getallusers,updateusers} from '../controllers/usermanagment/usermanagement';
+import {getallusers,updateusers,updatestatus} from '../controllers/usermanagment/usermanagement';
 const router = express.Router();
 
 
 
 router.get('/getallusers',getallusers);
 router.put('/updateusers/:id', updateusers);
+router.put('/updatestatus/:id', updatestatus);
 //updateusers
 
 
