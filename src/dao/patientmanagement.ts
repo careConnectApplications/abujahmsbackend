5103d8f0288bad248acfd903ef96f052463d8795
Malachi Chidiebere Egbugha <malachi.egbugha@ekedp.com>
2024-12-16 19:40:06 +0100

    adduploadtemplatesssss

--- src/dao/patientmanagement.ts
+++ src/dao/patientmanagement.ts
@@ -7,7 +7,7 @@ import configuration from "../config";
   export async function readallpatient(query:any) {
     try {
       const patientdetails = await Patient.find(query).select({"title":1,"firstName":1,"middleName":1,"lastName":1,"country":1, "stateOfResidence": 1,"LGA": 1,"address":1,"age":1,"dateOfBirth":1,"gender":1,"nin":1,"phoneNumber":1,"email":1,"oldMRN":1,"nextOfKinName":1,"nextOfKinRelatinship":1,"nextOfKinPhoneNumber":1,"nextOfKinAddress":1,
-    "maritalStatus":1, "disability":1,"occupation":1,"isHMOCover":1,"HMOName":1,"HMOId":1,"HMOPlan":1,"MRN":1,"createdAt":1});
+    "maritalStatus":1, "disability":1,"occupation":1,"isHMOCover":1,"HMOName":1,"HMOId":1,"HMOPlan":1,"MRN":1,"createdAt":1, "status": 1});
       const totalpatientdetails = await Patient.countDocuments();
       return { patientdetails, totalpatientdetails };
     } catch (err) {
