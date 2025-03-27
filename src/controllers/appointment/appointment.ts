4538047701f369129cc1e40f8fe0a3166c2d58e1
Malachi Chidiebere Egbugha <malachi.egbugha@ekedp.com>
2025-03-27 11:03:44 +0100

    addfinaltestss

--- src/controllers/appointment/appointment.ts
+++ src/controllers/appointment/appointment.ts
@@ -347,7 +347,7 @@ export const getAllPaidQueueSchedules = async (req:any, res:any) => {
     ,
    
     {
-      $match: { $or:[{'payment.status': configuration.status[3]},{'patient.isHMOCover':configuration.ishmo[1]}], status:configuration.status[5],clinic }  // Filter payment
+      $match: { $or:[{'payment.status': configuration.status[3]},{'patient.isHMOCover':configuration.ishmo[1]}], status:configuration.status[5],clinic,appointmentdate: { $gte: startOfDay, $lt: endOfDay } }  // Filter payment
       //$match: { 'patient.isHMOCover':configuration.ishmo[1], status:configuration.status[5],clinic,appointmentdate: { $gte: startOfDay, $lt: endOfDay } }  // Filter payment
     }
       
