858309d9d4b1418f81bf57b1b199911202c7be4c
Nzube Nnubia <ikezuby2012@gmail.com>
2025-08-01 08:54:31 +0100

    fix: fixed the parse date in creating anc3

--- src/utils/server.ts
+++ src/utils/server.ts
@@ -31,7 +31,7 @@ import settings from '../routes/setting';
 import theatreadmission from '../routes/theatreadmission';
 import users from "../routes/usermanagement";
 import { protect } from "../utils/middleware";
-import histopathologyText from  "../routes/histopathology-tests.route";
+import histopathologyText from "../routes/histopathology-tests.route";
 
 
 function createServer() {
@@ -49,6 +49,7 @@ function createServer() {
   app.use(express.static(__dirname + '/uploads'));
   //middleware to process json
   app.use(express.json({ limit: '50mb' }));
+  app.use(express.urlencoded({ extended: true }));
   /*
   app.use(fileUpload({
       useTempFiles: true,
@@ -85,8 +86,8 @@ function createServer() {
   app.use('/api/v1/readicdten', readicdeleven);
   app.use("/api/v1/histopathology", protect, histopathologyRoute)
   app.use("/api/v1/psychiatric", protect, psychiatricRoute)
-   app.use("/api/v1/histopathology-test", protect, histopathologyText);
-  
+  app.use("/api/v1/histopathology-test", protect, histopathologyText);
+
 
   // Handle POST requests to /webhook
   /*
