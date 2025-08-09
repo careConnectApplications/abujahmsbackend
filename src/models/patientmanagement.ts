31b04b39d385c515f0c6c8396967add5f7ca305d
Malachi Chidiebere Egbugha <malachi.egbugha@ekedp.com>
2025-08-09 13:10:44 +0100

    donewithpricingssss

--- src/models/patientmanagement.ts
+++ src/models/patientmanagement.ts
@@ -1,7 +1,7 @@
 import { Schema, model } from "mongoose";
 import configuration from "../config";
 import bcrypt from "bcryptjs";
-import { required } from "joi";
+
 export interface patientinterface {
   title: String;
   firstName: String;
