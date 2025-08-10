7ff78e802130735c2874c59002985bfd583036ef
Malachi Chidiebere Egbugha <malachi.egbugha@ekedp.com>
2025-08-10 16:46:54 +0100

    adjustappointmentdatess

--- src/controllers/histopathology/histopathology.controller.ts
+++ src/controllers/histopathology/histopathology.controller.ts
@@ -19,6 +19,7 @@ import catchAsync from "../../utils/catchAsync";
 import { IOptions } from "../../paginate/paginate";
 import pick from "../../utils/pick";
 import Histopathology from "../../models/histopathology";
+import {calculateAmountPaidByHMO} from  "../../utils/otherservices";
 
 const generateRefNumber = () => {
     const uniqueHistopathologyId = uuidv4();
@@ -78,7 +79,8 @@ export const CreateHistopatholgyService = catchAsync(async (req: Request | any, 
     if (!foundPatient) {
         return next(new ApiError(404, `Patient do not ${configuration.error.erroralreadyexit}`));
     }
-
+    var hmopercentagecover=foundPatient?.insurance?.hmopercentagecover ?? 0;
+    
     //const { servicetypedetails } = await readallservicetype({ category: configuration.category[6] }, { type: 1, category: 1, department: 1, _id: 0 });
 
     let totalAmount = 0;
@@ -93,8 +95,8 @@ export const CreateHistopatholgyService = catchAsync(async (req: Request | any, 
         if (!testPrice) {
             return next(new Error(`${configuration.error.errornopriceset}  ${service}`));
         }
-
-        const serviceAmount = testPrice.amount;
+        const serviceAmount =calculateAmountPaidByHMO(Number(hmopercentagecover), Number(testPrice.amount));
+        //const serviceAmount = testPrice.amount;
         totalAmount += serviceAmount;
 
         //const refNumber = generateRefNumber();
